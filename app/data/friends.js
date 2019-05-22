9516b86db6ec76bdf8bba3f34d024ab89cc54316
JoJo <shelton.jocelyn@gmail.com>
2019-05-21 23:01:47 -0400

    updated friend.js page

---
+++ app/data/friends.js
@@ -0,0 +1,106 @@
+var friendsArray = [
+
+	{
+		name: 'Ima Hogg',
+		photo: 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2018/07/funny-profile-pic62-768x512.jpg',
+		scores: [
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'5'
+		]
+    },
+
+    {
+		name: 'Chris P. Bacon',
+		photo: 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2018/07/funny-profile-pic100.jpg',
+		scores: [
+			'5',
+			'4',
+			'3',
+			'2',
+			'1',
+			'5',
+			'4',
+			'3',
+			'2',
+			'1'
+		]
+    },
+    
+    {
+		name: 'Mister Love',
+		photo: 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2018/07/funny-profile-pic13.jpg',
+		scores: [
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2'
+		]
+    },
+    
+    {
+		name: 'Tahra Dactyl',
+		photo: 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2018/07/funny-profile-pic80.jpg',
+		scores: [
+			'4',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'3'
+		]
+    },
+    
+    {
+		name: 'Donetta Duck',
+		photo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Amber%27sDuckFace.jpg/220px-Amber%27sDuckFace.jpg',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'5',
+			'1',
+			'2',
+			'3',
+			'4',
+			'5',
+			'1'
+		]
+    },
+    
+    {
+		name: 'Dyl Pickle',
+		photo: 'https://images.unsplash.com/photo-1551884831-bbf3cdc6469e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
+		scores: [
+			'3',
+			'3',
+			'1',
+			'1',
+			'2',
+			'2',
+			'4',
+			'4',
+			'5',
+			'1'
+		]
+    },
+];
+
+module.exports = friendsArray;
